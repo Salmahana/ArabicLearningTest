01ba6fd645335965d13f68c4e73ffcc3b3cd25b2
Aziza Toumi <flor_nor_2@hotmail.com>
2024-12-18 11:43:16 -0500

    adding () to letterScreens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack >
-      <Stack.Screen  name="mainScreen" />
       <Stack.Screen  name="index" />
-      <Stack.Screen name = '[letter]'/> 
+      <Stack.Screen name = '[letter]'options={{title:'الحروف'}}  /> 
 
     </Stack>
   );
